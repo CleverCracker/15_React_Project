e101f35bae4fdb6fa189ff1f2bbaf3efaf00b536
CleverCracker <clavercracker.gul@gmail.com>
2021-07-21 15:45:25 +0500

    2nd Project Initalization

--- 1_birthday_reminder/src/App.jsx
+++ 1_birthday_reminder/src/App.jsx
@@ -1,8 +1,18 @@
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
-
+import data from './data'
+import List from './List'
 function App() {
-  return <div className="App"></div>;
+  const [people,setPeople] = useState(data);
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} Birthday today</h3>
+        <List people={people} />
+        <button onClick={()=> setPeople([])}>Clear All</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
